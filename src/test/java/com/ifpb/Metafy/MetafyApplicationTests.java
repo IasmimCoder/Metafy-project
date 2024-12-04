19ef44bbca435e684ab740b09cc9f3fdc398ecb6
Iasmim Siqueira <iasmimsas@gmail.com>
2024-12-03 22:10:55 -0300

    Adiciona projeto Metafy

---
+++ src/test/java/com/ifpb/Metafy/MetafyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.Metafy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetafyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
