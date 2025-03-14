336bb0b940719d38b108c1536fba28043f847594
Eduardo9345 <jose.evangelista@academico.ifpb.edu.br>
2025-03-14 10:46:17 -0300

    Atualização JWT

--- src/main/java/com/ifpb/Metafy/service/UserService.java
+++ src/main/java/com/ifpb/Metafy/service/UserService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ifpb.Metafy.dto.UserDTO;
+import com.ifpb.Metafy.model.Role;
 import com.ifpb.Metafy.model.User;
 import com.ifpb.Metafy.repository.UserRepository;
 
@@ -71,5 +72,13 @@ public class UserService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmail(username);
     }
+
+    public UserDTO findDtoByEmail(String email) {
+		UserDTO user = userRepository.findDtoByEmail(email)
+				.orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado"));
+		List<Role> roles = userRepository.listRole(user.getId());
+		user.setRoles(roles);
+		return user;
+	}
 }
 
